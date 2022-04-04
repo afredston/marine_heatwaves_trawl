2a76c2881c4679534e52eddf182c747b99dce8cc
zoekitchel <zoe.kitchel@rutgers.edu>
2022-04-03 21:40:55 -0400

    next step polar map

--- map_hauls_global.R
+++ map_hauls_global.R
@@ -18,6 +18,8 @@ library(sf)
 library(cowplot)
 library(ggplot2)
 library(pals)
+library(concaveman)
+library(ggforce)
 
 #pacific centered base
 # install.packages("remotes")
@@ -34,41 +36,40 @@ haul_info[,longitude_s := ifelse(longitude > 150,(longitude-360),(longitude))]
 #delete if NA for longitude or latitude
 haul_info.r <- haul_info[complete.cases(haul_info[,.(longitude, latitude)])]
 
-#background world_map
-world <- ne_countries(scale = "medium", returnclass = "sf") #set up for world map
-class(world)
-
 #alternative
 
-library(rgdal)                                                                                                      
-library(raster)
-library(ggplot2)
-
-
 data("wrld_simpl", package = "maptools")                                                                            
-wm_polar <- crop(wrld_simpl, extent(-180, 180, 22, 90))                                                                   
+wm_polar <- crop(wrld_simpl, extent(-180, 180, 22, 90))  
+
+# Defines the x axes required
+x_lines <- seq(-120,180, by = 60)
 
 #polar
 basemap_polar <- ggplot() +
-  geom_polygon(data = wm, aes(x = long, y = lat, group = group), fill = "azure4", 
+  geom_polygon(data = wm_polar, aes(x = long, y = lat, group = group), fill = "azure4", 
               # colour = "black"
               #,
               # alpha = 0.8
               ) +
   
+  # Adds axes
+  geom_hline(aes(yintercept = 22), size = 1)  +
+  geom_segment(aes(y = 22, yend = 90, x = x_lines, xend = x_lines), linetype = "dashed", alpha = 0.3) +
+  
   # Convert to polar coordinates
   coord_map("ortho", orientation = c(50, -50, -20)) +
   scale_y_continuous(breaks = seq(0, 90, by = 5), labels = NULL) +
   
-  # Removes Axes and labels
-  scale_x_continuous(breaks = NULL) +
-  xlab("") + 
-  ylab("") +
+  
+  #axis
+  geom_text(aes(x = x_lines, y = 15, label = c("120°W", "60°W", "0°", "60°E", "120°E", "180°W"))) +
   
   # Change theme to remove axes and ticks
-  theme(panel.background = element_blank(),
-        axis.ticks=element_blank())
-
+theme_classic() +
+  theme(axis.text = element_blank(), axis.title = element_blank(),
+        axis.line = element_blank(), axis.ticks = element_blank())
+  
+  
 ##########
 #distinctive color pallette
 survey_pallette <- c("#AAF400","#B5EFB5","#F6222E","#FE00FA", 
@@ -83,4 +84,82 @@ survey_regions_polar <- basemap_polar + geom_point(data = haul_info.r, aes(x = l
 #save global map
 ggsave(survey_regions_polar, path = here::here("figures"), filename = "survey_regions_polar.jpg",height = 5, width = 6, unit = "in")
 
+###polygons instead of points
+#leave out AI for now
+haul_info.r.noAI <- haul_info.r[survey != "AI",]
+haul_info.r.AI <- haul_info.r[survey == "AI",]
+
+#split points into list all but ai
+haul_info.r.split <- split(haul_info.r.noAI, haul_info.r.noAI$survey)
+haul_info.r.split.sf <- lapply(haul_info.r.split, st_as_sf, coords = c("longitude", "latitude"))
+haul_info.r.split.concave <- lapply(haul_info.r.split.sf, concaveman, concavity = 3, length_threshold = 2)
+haul_info.r.split.concave.binded <- do.call('rbind', haul_info.r.split.concave)
+haul_info.r.split.concave.binded.spdf <- as_Spatial(haul_info.r.split.concave.binded)
+
+#split AI points into list
+haul_info.r.AI.split <- split(haul_info.r.AI, haul_info.r.AI$survey)
+haul_info.r.AI.split.sf <- lapply(haul_info.r.AI.split, st_as_sf, coords = c("longitude_s", "latitude"))
+haul_info.r.AI.split.concave <- lapply(haul_info.r.AI.split.sf, concaveman, concavity = 5, length_threshold = 2)
+haul_info.r.AI.split.concave.binded <- do.call('rbind', haul_info.r.AI.split.concave)
+haul_info.r.AI.split.concave.binded.spdf <- as_Spatial(haul_info.r.AI.split.concave.binded)
+
+
+#merge
+haul_info.r.split.concave.binded_merge <- rbind(haul_info.r.AI.split.concave.binded,
+                                                   haul_info.r.split.concave.binded)
+
+haul_info.r.split.concave.binded_merge.spdf <- as_Spatial(haul_info.r.AI.split.concave.binded_merge)
+
+haul_info.r.split.concave.binded_merge.spdf$survey <- levels(as.factor(haul_info.r$survey))
+
+
+#add these polygons to map
+#add survey polygons
+#and rearrange order
+
+survey_regions_polar_polygon <- ggplot() +
+  geom_polygon(data = haul_info.r.split.concave.binded_merge.spdf,
+               aes(x = long, y = lat, group = group, fill = group, color = group),
+               alpha = 0.8) +
+  scale_color_manual(values = survey_pallette) +
+  scale_fill_manual(values = survey_pallette)  +
+  geom_polygon(data = wm_polar, aes(x = long, y = lat, group = group), fill = "azure4", 
+               # colour = "black"
+               #,
+               # alpha = 0.8
+  ) +
+  
+  # Adds axes
+  geom_hline(aes(yintercept = 22), size = 1)  +
+  geom_segment(aes(y = 22, yend = 90, x = x_lines, xend = x_lines), linetype = "dashed", alpha = 0.3) +
+  
+  # Convert to polar coordinates
+  coord_map("ortho", orientation = c(50, -50, -20)) +
+  scale_y_continuous(breaks = seq(0, 90, by = 5), labels = NULL) +
+  
+  
+  #axis
+  geom_text(aes(x = x_lines, y = 15, label = c("120°W", "60°W", "0°", "60°E", "120°E", "180°W"))) +
+  
+  # Change theme to remove axes and ticks
+  theme_classic() +
+  theme(axis.text = element_blank(), axis.title = element_blank(),
+        axis.line = element_blank(), axis.ticks = element_blank(),
+        legend.position = "none")
+
+#save global map
+ggsave(survey_regions_polar_polygon, path = here::here("figures"),
+       filename = "survey_regions_polar_polygon.jpg",height = 5, width = 6, unit = "in")
+
+
+
+survey_regions_polar_polygon <- basemap_polar +
+  geom_polygon(data = haul_info.r.split.concave.binded_merge.spdf,
+          aes(x = long, y = lat, group = group, fill = group, color = group),
+               alpha = 0.8) +
+  scale_color_manual(values = survey_pallette) +
+  scale_fill_manual(values = survey_pallette)  +  
+  theme(legend.position = "none")
+
+#what abouttt
 
