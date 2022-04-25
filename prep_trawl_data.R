69c8f075886b8945d52791220013b076555e0ca1
Alexa Fredston <alexa.fredstonhermann@gmail.com>
2022-04-25 12:49:08 -0400

    finished spatial standardization

--- prep_trawl_data.R
+++ prep_trawl_data.R
@@ -56,7 +56,7 @@ gmex_hauls_del <- unique(raw[survey=='GMEX' & year %in% c(1987,2020), haul_id]) 
 gsl_s_hauls_del <-  unique(raw[survey=='GSL-S' & year < 1985, haul_id]) # GSL-S is also missing 2003 data which shouldn't be an issue, just introduces one 2-year gap 
 neus_hauls_del <- unique(raw[survey=='NEUS' & (year < 1968 | year > 2019), haul_id])
 nigfs_hauls_del <- unique(raw[survey=='NIGFS' & year < 2009, haul_id])
-nor_bts_hauls_del <- unique(raw[survey=='Nor-BTS' & year == 1980, haul_id]) # rest of the time-series has a lot of fluctuation too
+nor_bts_hauls_del <- c(unique(raw[survey=='Nor-BTS' & year < 1981, haul_id]), unique(raw[survey=='Nor-BTS' & year > 2005, haul_id]), unique(raw[survey=='Nor-BTS' & latitude<68, haul_id])) # rest of the time-series has a lot of fluctuation and very few hauls. latitude cutoff to exclude the norwegian sea, which is rarely sampled, and focus on the barents sea 
 ns_ibts_hauls_del <- unique(raw[survey=='NS-IBTS' & year < 1980, haul_id]) # this is a somewhat arbitrary cutoff -- hauls/yr just increases linearly with time here -- but at least we're discarding the years with very few hauls
 pt_ibts_hauls_del <- unique(raw[survey=='PT-IBTS' & year %in% c(2002, 2018), haul_id]) # also missing 2012 data, shouldn't be an issue, just introduces one 2-year gap 
 scs_hauls_del <- unique(raw[survey=='SCS' & (year < 1979 | year > 2017), haul_id])
@@ -110,41 +110,59 @@ raw[,startmonth := min(month), by=survey]
 # add special cases back in 
 raw <- rbind(raw, bits_raw, ebs_raw, evhoe_raw, fr_cgfs_raw, gmex_raw, goa_raw, ie_igfs_raw, ns_ibts_raw, neus_raw, nigfs_raw, nor_bts_raw, scs_raw, seus_raw, swc_ibts_raw)
 
-# update haulinfo and add startmonth column
+# update haulinfo 
 haul_info <- copy(haul_info)[haul_id %in% raw$haul_id]
-haul_info %<>% merge(unique(raw[,.(survey, startmonth)]), all.x=TRUE, by="survey")
+# haul_info %<>% merge(unique(raw[,.(survey, startmonth)]), all.x=TRUE, by="survey") # this gets added to raw so don't need to put it in haul_info, they're merged at the end anyway
 
+# STANDARDIZE SURVEY FOOTPRINTS 
 
-# STANDARDIZE SURVEY FOOTPRINTS (M. Pinsky)
-
-FishGlob <- copy(raw)[,longitude_s := ifelse(longitude > 150,(longitude-360),(longitude))]
+FishGlob <- copy(raw)[,longitude_s := ifelse(longitude > 180,(longitude-360),(longitude))]
 
 #delete if NA for longitude or latitude
 FishGlob <- FishGlob[complete.cases(FishGlob[,.(longitude, latitude)])]
+FishGlob_nor <- FishGlob[survey=='Nor-BTS']
+FishGlob_else <- FishGlob[!survey=='Nor-BTS']
+rm(FishGlob)
 
 #set up grid
-dggs <- dgconstruct(res = 8, metric = T) #with res = 8, we will need at least 3 observations per year within 7,774.2 km^2 (roughly size of some NEUS strata)
+dggs8 <- dgconstruct(res = 8, metric = T) #with res = 8, we will need at least 3 observations per year within 7,774.2 km^2 (roughly size of some NEUS strata)
+dggs7 <- dgconstruct(res = 7, metric = T) # for Norway only 
 
 #pull out unique lat lons and get grid cells
-unique_latlon <- unique(FishGlob[,.(latitude, longitude_s)])
-unique_latlon[,cell := dgGEO_to_SEQNUM(dggs, longitude_s, latitude)] #get corresponding grid cells for this region/survey combo
+unique_latlon_nor <- unique(FishGlob_nor[,.(latitude, longitude_s)])
+unique_latlon_else <- unique(FishGlob_else[,.(latitude, longitude_s)])
+unique_latlon_else[,cell := dgGEO_to_SEQNUM(dggs8, longitude_s, latitude)] #get corresponding grid cells for this region/survey combo
+unique_latlon_nor[,cell := dgGEO_to_SEQNUM(dggs7, longitude_s, latitude)] #get corresponding grid cells for this region/survey combo
 
 #find cell centers
-cellcenters <- dgSEQNUM_to_GEO(dggs, unique_latlon[,cell])
+cellcenters_else <- dgSEQNUM_to_GEO(dggs8, unique_latlon_else[,cell])
+cellcenters_nor <- dgSEQNUM_to_GEO(dggs7, unique_latlon_nor[,cell])
 
 #linking cell centers to unique_latlon
-unique_latlon[,cell_center_longitude_s := cellcenters$lon_deg][,cell_center_latitude:= cellcenters$lat_deg]
+unique_latlon_else[,cell_center_longitude_s := cellcenters_else$lon_deg][,cell_center_latitude:= cellcenters_else$lat_deg]
+unique_latlon_nor[,cell_center_longitude_s := cellcenters_nor$lon_deg][,cell_center_latitude:= cellcenters_nor$lat_deg]
 
 #link centers back to main data table
-FishGlob.dg <- merge(FishGlob, unique_latlon, by = c("latitude", "longitude_s"), all.x = TRUE)
+FishGlob_else.dg <- merge(FishGlob_else, unique_latlon_else, by = c("latitude", "longitude_s"), all.x = TRUE)
+FishGlob_else.dg$dggs <- "8"
+FishGlob_nor.dg <- merge(FishGlob_nor, unique_latlon_nor, by = c("latitude", "longitude_s"), all.x = TRUE)
+FishGlob_nor.dg$dggs <- "7"
+
+FishGlob.dg <- rbind(FishGlob_else.dg, FishGlob_nor.dg)
+
+# check that they have the same number of rows and replace raw with the data.table that has spatial standardization columns
+if(nrow(FishGlob.dg==nrow(raw))){
+  raw <- copy(FishGlob.dg)
+  rm(FishGlob.dg)
+}
 
 # make a list of all unique year x cell x survey 
 
 # iterate over regions because some surveys fall in the same cells 
-survs <- FishGlob.dg[, unique(survey)]
+survs <- raw[, unique(survey)]
 year_cell_count.dt <- NULL
 for(i in 1:length(survs)) {
-  tmp <- FishGlob.dg[survey==survs[i]]
+  tmp <- raw[survey==survs[i]]
   cell_years <- as.data.table(expand.grid(cell = unique(tmp$cell), 
                                           year = unique(tmp$year)))
   cell_years$survey <- survs[i]
@@ -152,7 +170,7 @@ for(i in 1:length(survs)) {
 }
 
 # calc num hauls per year x cell x survey, then merge to full list
-temp <- FishGlob.dg[, .(nhaul = length(unique(haul_id))), by = .(cell, year, survey)]
+temp <- raw[, .(nhaul = length(unique(haul_id))), by = .(cell, year, survey)]
 year_cell_count.dt <- merge(year_cell_count.dt, temp, all.x = TRUE)
 year_cell_count.dt[is.na(nhaul), nhaul := 0] # fill in for year x cell x survey that aren't present
 # year_cell_count.dt[, years_sampled := uniqueN(year[nhaul>0]), by=.(survey, cell)] # count up years when there were samples 
@@ -161,77 +179,115 @@ year_cell_count.dt[is.na(nhaul), nhaul := 0] # fill in for year x cell x survey 
 # year_cell_count.dt[, survcell := paste0(survey, cell)]
 # length(unique(year_cell_count.dt$survcell)) # 1159
 # moop <- copy(year_cell_count.dt)[prop_sampled>=0.9]
-# 
-# loop through each survey. Couldn't figure out how to do this without a loop.
-plots <- vector('list', length(survs))
+
+year_cell_count.dt[, max_years := length(unique(year)), by=survey]
+year_cell_count_summ <- year_cell_count.dt[nhaul>0, .(sampled_years = length(unique(year))), by=c("cell","survey","max_years")]
+year_cell_count_summ[, prop_sampled := sampled_years / max_years]
+ggplot(year_cell_count.dt[, .(nhauls = sum(nhaul)), by=c("year","survey")]) +
+  geom_line(aes(x=year, y=nhauls)) +
+  facet_wrap(~survey)
+
+# figure out how many cells would be dropped by trimming to those sampled in every year 
+trim.dt <- NULL
 for(i in 1:length(survs)){
-  # trim to this survey
-  thisyearcellcount <- year_cell_count.dt[survey == survs[i],]
-
-  # calculate the most to least sampled cells and years
-  year_order <- thisyearcellcount[nhaul >0, .(ncell = length(unique(cell))),
-                                  by = year][ncell >0,][order(ncell),]
-  cell_order <- thisyearcellcount[nhaul > 0, .(nyear = length(unique(year))),
-                                  by = cell][nyear >0,][order(nyear),]
-
-  # set the factor order by ave number of hauls
-  thisyearcellcount[, year := factor(year, levels = year_order$year)]
-  thisyearcellcount[, cell := factor(cell, levels = cell_order$cell)]
-
-  # calculate num missing cells x years for different thresholds. slow.
-  cutoffs <- data.table(expand.grid(year = levels(thisyearcellcount$year), cell = levels(thisyearcellcount$cell)))
-  cutoffs[, ':='(ntot = NA_integer_, nmiss = NA_integer_, nkeep = NA_integer_)]
-  nyr <- length(unique(cutoffs$year))
-  ncl <- length(unique(cutoffs$cell))
-  for(j in 1:nyr){
-    for(k in 1:ncl){
-      # ranks cell*year combinations by missingness 
-      thisntot <- thisyearcellcount[cell %in% levels(cell)[k:ncl] & year %in% levels(year)[j:nyr], .N]
-      thisnmiss <- thisyearcellcount[cell %in% levels(cell)[k:ncl] & year %in% levels(year)[j:nyr], sum(nhaul == 0)]
-      thisnkeep <- thisyearcellcount[cell %in% levels(cell)[k:ncl] & year %in% levels(year)[j:nyr], sum(nhaul > 0)]
-      cutoffs[cell == thisyearcellcount[, levels(cell)[k]] &
-                year == thisyearcellcount[,levels(year)[j]],
-              ':='(ntot = thisntot, nmiss = thisnmiss, nkeep = thisnkeep)]
-    }
-  }
-
-  # choose a threshold
-  chosencutoffs0 <- cutoffs[nmiss==0,][nkeep == max(nkeep),] # based on nothing missing
-  chosencutoffs02 <- cutoffs[nmiss/ntot < 0.02,][nkeep == max(nkeep),] # based on <2% missing
-
-
-# print out the cells and years to keep
-print(paste0('For ', survs[i], ' 0% missing'))
-print(paste0('Keep these years:',
-             paste0(thisyearcellcount[, levels(year)[as.numeric(chosencutoffs0$year):nyr]], collapse=',')))
-print(paste0('Keep these cells:',
-             paste0(thisyearcellcount[, levels(cell)[as.numeric(chosencutoffs0$cell):ncl]], collapse = ',')))
-print(paste0('Drop these years:',
-             paste0(thisyearcellcount[, levels(year)[1:as.numeric(chosencutoffs0$year)]], collapse=',')))
-print(paste0('Drop these cells:',
-             paste0(thisyearcellcount[, levels(cell)[1:as.numeric(chosencutoffs0$cell)]], collapse = ',')))
-
-print(paste0('For ', survs[i], ' 2% missing'))
-print(paste0('Keep these years:',
-             paste0(thisyearcellcount[, levels(year)[as.numeric(chosencutoffs02$year):nyr]], collapse = ',')))
-print(paste0('Keep these cells:',
-             paste0(thisyearcellcount[, levels(cell)[as.numeric(chosencutoffs02$cell):ncl]], collapse = ',')))
-print(paste0('Drop these years:',
-             paste0(thisyearcellcount[, levels(year)[1:as.numeric(chosencutoffs02$year)]], collapse=',')))
-print(paste0('Drop these cells:',
-             paste0(thisyearcellcount[, levels(cell)[1:as.numeric(chosencutoffs02$cell)]], collapse = ',')))
-
-  # make plot
-  # plots[[i]] <- ggplot(thisyearcellcount[nhaul > 0,], aes(year, cell, color = nhaul)) +
-  #   geom_point() +
-  #   theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 4),
-  #         axis.text.y = element_text(size = 5)) +
-  #   labs(title = survs[i]) +
-  #   geom_vline(xintercept = chosencutoffs0[, as.numeric(year)], color = 'red') +
-  #   geom_hline(yintercept = chosencutoffs0[, as.numeric(cell)], color = 'red') +
-  #   geom_vline(xintercept = chosencutoffs02[, as.numeric(year)], color = 'purple') +
-  #   geom_hline(yintercept = chosencutoffs02[, as.numeric(cell)], color = 'purple')
+  summtmp <- year_cell_count_summ[survey==survs[i]]
+  dttmp <- year_cell_count.dt[survey==survs[i]]
+ # print(paste0("In region ",survs[i]," keeping cells sampled in 100% of years would keep ",length(unique(summtmp[prop_sampled==1]$cell))," of ",length(unique(summtmp$cell))," cells and ",sum(dttmp[cell %in% summtmp[prop_sampled==1]$cell]$nhaul)," of ",sum(dttmp$nhaul)," hauls"))
+  year_cell_count.dt[survey == survs[i] & cell %in% summtmp[prop_sampled==1]$cell, keep_100 := TRUE]
+  year_cell_count.dt[survey == survs[i] & !cell %in% summtmp[prop_sampled==1]$cell, keep_100 := FALSE]
+  year_cell_count.dt[survey == survs[i] & cell %in% summtmp[prop_sampled>=0.95]$cell, keep_95 := TRUE]
+  year_cell_count.dt[survey == survs[i] & !cell %in% summtmp[prop_sampled>=0.95]$cell, keep_95 := FALSE]
+  year_cell_count.dt[survey == survs[i] & cell %in% summtmp[prop_sampled>=0.9]$cell, keep_90 := TRUE]
+  year_cell_count.dt[survey == survs[i] & !cell %in% summtmp[prop_sampled>=0.9]$cell, keep_90 := FALSE] 
+  trim.dt <- rbind(trim.dt, copy(year_cell_count.dt[survey==survs[i]])[, .(ncells = length(unique(cell)), nyears = length(unique(year)), 
+                                                            prop_cells_100 = length(unique(summtmp[prop_sampled==1]$cell))/length(unique(cell)),
+                                                            prop_cells_95 = length(unique(summtmp[prop_sampled>=0.95]$cell))/length(unique(cell)),
+                                                            prop_cells_90 = length(unique(summtmp[prop_sampled>=0.9]$cell))/length(unique(cell)),
+                                                            prop_hauls_100 = sum(year_cell_count.dt[survey==survs[i] & cell %in% summtmp[prop_sampled==1]$cell]$nhaul)/sum(nhaul),
+                                                            prop_hauls_95 = sum(year_cell_count.dt[survey==survs[i] & cell %in% summtmp[prop_sampled>=0.95]$cell]$nhaul)/sum(nhaul),
+                                                            prop_hauls_90 = sum(year_cell_count.dt[survey==survs[i] & cell %in% summtmp[prop_sampled>=0.9]$cell]$nhaul)/sum(nhaul)),by=survey])
 }
+
+# explore Norway missingness 
+# boop <- FishGlob_nor.dg[, .(nhaul = length(unique(haul_id))), by=.(cell_center_longitude_s, cell_center_latitude, cell)]
+# boop <- merge(boop, summtmp, by="cell", all.x=TRUE)
+# ggplot() + geom_point(data=boop, aes(x=cell_center_longitude_s, y=cell_center_latitude, fill=nhaul, color=nhaul),size=1) + scale_color_viridis_c() + scale_fill_viridis_c() + geom_polygon(data=map_data("world"), aes(x=long, y=lat,group=group)) + coord_cartesian(xlim=c(-21, 71), ylim=c(60, 85))
+# moop <- year_cell_count.dt[survey=='Nor-BTS']
+# moop %<>% merge(., unique(unique_latlon_nor[, .(cell, cell_center_longitude_s, cell_center_latitude)]), by="cell", keep.x='TRUE')
+# moop[.(nhaul = 0, to = NA), on = "nhaul", nhaul := i.to] # recode zeros to NAs for plotting
+# ggplot() + geom_point(data=moop, aes(x=cell_center_longitude_s, y=cell_center_latitude, fill=nhaul),size=1.5, shape=21) + geom_polygon(data=map_data("world"), aes(x=long, y=lat,group=group)) + coord_cartesian(xlim=c(-21, 71), ylim=c(60, 85)) + facet_wrap(~year) + scale_fill_gradient(low="lightskyblue",high="firebrick3", na.value="white")
+# 
+
+# loop through each survey. Couldn't figure out how to do this without a loop.
+# plots <- vector('list', length(survs))
+# for(i in 1:length(survs)){
+#   # trim to this survey
+#   thisyearcellcount <- year_cell_count.dt[survey == survs[i],]
+# 
+#   # calculate the most to least sampled cells and years
+#   year_order <- thisyearcellcount[nhaul >0, .(ncell = length(unique(cell))),
+#                                   by = year][ncell >0,][order(ncell),]
+#   cell_order <- thisyearcellcount[nhaul > 0, .(nyear = length(unique(year))),
+#                                   by = cell][nyear >0,][order(nyear),]
+# 
+#   # set the factor order by ave number of hauls
+#   thisyearcellcount[, year := factor(year, levels = year_order$year)]
+#   thisyearcellcount[, cell := factor(cell, levels = cell_order$cell)]
+# 
+#   # calculate num missing cells x years for different thresholds. slow.
+#   cutoffs <- data.table(expand.grid(year = levels(thisyearcellcount$year), cell = levels(thisyearcellcount$cell)))
+#   cutoffs[, ':='(ntot = NA_integer_, nmiss = NA_integer_, nkeep = NA_integer_)]
+#   nyr <- length(unique(cutoffs$year))
+#   ncl <- length(unique(cutoffs$cell))
+#   for(j in 1:nyr){
+#     for(k in 1:ncl){
+#       # ranks cell*year combinations by missingness 
+#       thisntot <- thisyearcellcount[cell %in% levels(cell)[k:ncl] & year %in% levels(year)[j:nyr], .N]
+#       thisnmiss <- thisyearcellcount[cell %in% levels(cell)[k:ncl] & year %in% levels(year)[j:nyr], sum(nhaul == 0)]
+#       thisnkeep <- thisyearcellcount[cell %in% levels(cell)[k:ncl] & year %in% levels(year)[j:nyr], sum(nhaul > 0)]
+#       cutoffs[cell == thisyearcellcount[, levels(cell)[k]] &
+#                 year == thisyearcellcount[,levels(year)[j]],
+#               ':='(ntot = thisntot, nmiss = thisnmiss, nkeep = thisnkeep)]
+#     }
+#   }
+# 
+#   # choose a threshold
+#   chosencutoffs0 <- cutoffs[nmiss==0,][nkeep == max(nkeep),] # based on nothing missing
+#   chosencutoffs02 <- cutoffs[nmiss/ntot < 0.02,][nkeep == max(nkeep),] # based on <2% missing
+# 
+# 
+# # print out the cells and years to keep
+# print(paste0('For ', survs[i], ' 0% missing'))
+# print(paste0('Keep these years:',
+#              paste0(thisyearcellcount[, levels(year)[(as.numeric(chosencutoffs0$year)+1):nyr]], collapse=',')))
+# print(paste0('Keep these cells:',
+#              paste0(thisyearcellcount[, levels(cell)[(as.numeric(chosencutoffs0$cell)+1):ncl]], collapse = ',')))
+# print(paste0('Drop these years:',
+#              paste0(thisyearcellcount[, levels(year)[1:as.numeric(chosencutoffs0$year)]], collapse=',')))
+# print(paste0('Drop these cells:',
+#              paste0(thisyearcellcount[, levels(cell)[1:as.numeric(chosencutoffs0$cell)]], collapse = ',')))
+# 
+# print(paste0('For ', survs[i], ' 2% missing'))
+# print(paste0('Keep these years:',
+#              paste0(thisyearcellcount[, levels(year)[(as.numeric(chosencutoffs02$year)+1):nyr]], collapse = ',')))
+# print(paste0('Keep these cells:',
+#              paste0(thisyearcellcount[, levels(cell)[(as.numeric(chosencutoffs02$cell)+1):ncl]], collapse = ',')))
+# print(paste0('Drop these years:',
+#              paste0(thisyearcellcount[, levels(year)[1:as.numeric(chosencutoffs02$year)]], collapse=',')))
+# print(paste0('Drop these cells:',
+#              paste0(thisyearcellcount[, levels(cell)[1:as.numeric(chosencutoffs02$cell)]], collapse = ',')))
+# 
+#   #make plot
+#   plots[[i]] <- ggplot(thisyearcellcount[nhaul > 0,], aes(year, cell, color = nhaul)) +
+#     geom_point() +
+#     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1, size = 4),
+#           axis.text.y = element_text(size = 5)) +
+#     labs(title = survs[i]) +
+#     geom_vline(xintercept = chosencutoffs0[, as.numeric(year)], color = 'red') +
+#     geom_hline(yintercept = chosencutoffs0[, as.numeric(cell)], color = 'red') +
+#     geom_vline(xintercept = chosencutoffs02[, as.numeric(year)], color = 'purple') +
+#     geom_hline(yintercept = chosencutoffs02[, as.numeric(cell)], color = 'purple')
+# }
 
 # plots
 #do.call("grid.arrange", c(plots, ncol=5))
@@ -239,6 +295,9 @@ print(paste0('Drop these cells:',
 ##########
 # cut down raw to the good hauls in haul_info, and expand with zeros
 ##########
+haul_info %<>% merge(unique(raw[,.(haul_id, cell)]), by="haul_id", all.x=TRUE) # add cell column; could add other cell attributes here if wanted
+haul_info %<>% merge(year_cell_count.dt, by=c("cell","year","survey"), all.x=TRUE) # get keep_90 column
+haul_info <- haul_info[keep_90==TRUE] # get hauls that pass spatial standardization filter 
 
 raw <- copy(raw)[, .(survey, haul_id, wgt_cpue, accepted_name, startmonth)][haul_id %in% haul_info$haul_id] # trim to only taxon-level data for speed (but note there is a full taxonomy available, and other catch data), and to hauls in haul_info
 
@@ -262,7 +321,6 @@ raw_zeros <- copy(raw_zeros)[is.na(wgt_cpue), wgt_cpue := 0][wgt_cpue<Inf] # rep
 #########
 # Tidy data
 #########
-
 
 # are there any regions missing a lot of depth data?
 # tmp <- copy(raw_zeros)[is.na(depth)]
@@ -288,3 +346,4 @@ raw_cpue %<>% merge(raw_depth, all.x=TRUE, by=c("survey", "accepted_name", "year
 #########
 fwrite(raw_cpue, here("processed-data","biomass_time.csv"))
 fwrite(haul_info, here("processed-data","haul_info.csv"))
+fwrite(trim.dt, here("processed-data","spatial_standardization_summary.csv"))
