ec8c70afd5ef67201230385973f0de8007b93e76
Alexa Fredston <alexa.fredstonhermann@gmail.com>
2022-06-10 16:40:16 +0300

    finished(ish) power analysis

--- power_analysis.R
+++ power_analysis.R
@@ -4,6 +4,8 @@ library(here)
 library(doParallel)
 detectCores()
 registerDoParallel(cores=4)
+mhw_summary_sat_sst_5_day <- read_csv(here("processed-data","mhw_satellite_sst_5_day_threshold.csv")) # MHW summary stats from satellite SST record, defining MHWs as events >=5 days
+
 survey_summary <-read_csv(here("processed-data","survey_biomass_with_CTI.csv")) %>% inner_join(mhw_summary_sat_sst_5_day)
 survey_names <- read_csv(here('processed-data','survey_names.csv'))
 
@@ -23,7 +25,7 @@ trialyrs <- seq(10, 100, 10)
 # based on the effect sizes in Cheung et al. (6% overall biomass loss in worst-case scenario), how much data would we have needed, given the actual variance in the data? 
 
 # create a function that takes a dataset, a list of surveys, and a list of year lengths, and simulates MHW effects in each survey for that number of years based on Cheung et al.'s estimate of true biomass effects of MHWs 
-sim_power_by_yr <- function(powerdat, surv, trialyrs, a){
+fn_sim_yr <- function(powerdat, surv, trialyrs, a){
   
   Data = powerdat %>% filter(survey==surv)
   Gompertz = lm( log(wt) ~ 1 + log(lagwt) + mhw_yes_no, data=Data )
@@ -42,7 +44,7 @@ sim_power_by_yr <- function(powerdat, surv, trialyrs, a){
   marginal_sd = conditional_sd / (1-beta)^2
   marginal_mean = alpha / (1-beta)
   
-  pwr_results <- NULL
+  sim_yrs <- NULL
   
   for(j in trialyrs){
     n_years = j
@@ -64,26 +66,26 @@ sim_power_by_yr <- function(powerdat, surv, trialyrs, a){
       arrange(year) %>% 
       mutate(wt_mt_log = log(wt / lag(wt))) 
     
-    pwr_results <- rbind(pwr_results, tmp)
+    sim_yrs <- rbind(sim_yrs, tmp)
   } # close j loop (nyears)
   
-  return(pwr_results)
+  return(sim_yrs)
 }
 
 # parallelize implementing this function over iterations, year lengths, and surveys
 
-pwrout <- foreach(surv=survey_names$survey, .combine='rbind') %:% 
+pwrout_yrs <- foreach(surv=survey_names$survey, .combine='rbind') %:% 
   foreach(a = seq(1, iters, 1), .combine='rbind') %dopar% {
-    sim_power_by_yr(powerdat=powerdat, surv=surv, trialyrs=trialyrs, a=a)
+    fn_sim_yr(powerdat=powerdat, surv=surv, trialyrs=trialyrs, a=a)
   }
 # is it the right size?
-(iters * sum(trialyrs) * nrow(survey_names)) == nrow(pwrout)
+(iters * sum(trialyrs) * nrow(survey_names)) == nrow(pwrout_yrs)
 
 # analyze simulated data 
 sim_test <- NULL
 for(j in trialyrs){
   for(i in 1:iters){
-    tmp10 <- pwrout %>% 
+    tmp10 <- pwrout_yrs %>% 
       filter(n_years==j,
              iter==i) 
     tmp10_no <- tmp10 %>% filter(wt_mt_log > -Inf, wt_mt_log < Inf, mhw_yes_no == 0) %>% pull(wt_mt_log)
@@ -103,10 +105,12 @@ sim_test_summ <- sim_test %>%
   summarise(propsig <- length(p.value[p.value<=0.05])/length(p.value))
 
 # PART 2: given the data we have, what effect size could we detect? 
+# note that unlike the function above, which applies the same duration to each survey, this holds each survey at its actual number of sample-years and just varies the true effect size to see what we would have detected with our methods 
 
-effectout <- NULL
-for(surv in survey_names$survey){
+fn_sim_gamma <- function(powerdat, surv, gammas, a){
+  
   Data = powerdat %>% filter(survey==surv)
+  n_years = length(unique(Data$year))
   Gompertz = lm( log(wt) ~ 1 + log(lagwt) + mhw_yes_no, data=Data )
   
   # Gompertz parameters
@@ -114,49 +118,70 @@ for(surv in survey_names$survey){
   beta = Gompertz$coef['log(lagwt)']
   conditional_sd = sqrt(mean(Gompertz$residuals^2))
   
-  #
-  n_years = length(unique(Data$year))
-  
   # MHW frequency and intensity
   prob_mhw = mean( ifelse(Data[,'mhw_yes_no']=="yes",1,0), na.rm=TRUE )
-  
   gamma = log(0.90) # vary to evaluate how much of a decrease we could detect 
+  
   
   # Stationary properties (for initial condition)
   marginal_sd = conditional_sd / (1-beta)^2
   marginal_mean = alpha / (1-beta)
   
-  #
+  sim_gamma <- NULL
+  
   logB_t = rep(NA,n_years)
   MHW_t = rbinom(n_years, size=1, prob=prob_mhw)
   
   # Initialize
-  #  logB_t[1] = rnorm( n=1, mean=marginal_mean, sd=marginal_sd )
-  logB_t[1] = mean = marginal_mean # variance was too big for initializing 
+  # logB_t[1] = rnorm( n=1, mean=marginal_mean, sd=marginal_sd )
+  logB_t[1] = marginal_mean # variance was too big for initializing 
   
-  # Project
-  #  Gompertz:  log(N(t+1)) = alpha + beta * log(N(t)) + effects + error
+  for(g in gammas){
   for( tI in 2:n_years){
-    logB_t[tI] = alpha + beta*logB_t[tI-1] + gamma*MHW_t[tI] + rnorm( n=1, mean=0, sd=conditional_sd )
+    logB_t[tI] = alpha + beta*logB_t[tI-1] + g*MHW_t[tI] + rnorm( n=1, mean=0, sd=conditional_sd )
   }
   
-  tmp <- tibble(wt = exp(logB_t), year = seq(1, n_years, 1), mhw_yes_no = MHW_t, n_years = n_years, gamma = gamma, survey = unique(Data$survey))
+  # set up dataframe to write out the results 
+  tmp <- tibble(wt = exp(logB_t), year = seq(1, n_years, 1), mhw_yes_no = MHW_t, n_years = n_years, gamma = g, survey = unique(Data$survey), iter = a) %>% 
+    arrange(year) %>% 
+    mutate(wt_mt_log = log(wt / lag(wt))) 
   
-  effectout <- rbind(effectout, tmp)
+  sim_gamma <- rbind(sim_gamma, tmp)
+  }
+  return(sim_gamma)
 }
 
-effecttest <- effectout %>% 
-  group_by(survey) %>% 
-  arrange(year) %>% 
-  mutate(wt_mt_log = log(wt / lag(wt))) %>% 
-  filter(wt_mt_log > -Inf, wt_mt_log < Inf) 
 
-effecttest_mhw <- effecttest %>% 
-  filter(mhw_yes_no == 1) %>% 
-  pull(wt_mt_log)
+# parallelize implementing this function over iterations, gamma values, and surveys
 
-effecttest_no_mhw <- effecttest %>% 
-  filter(mhw_yes_no == 0) %>% 
-  pull(wt_mt_log)
+# theoretical biomass loss from 50% to 5%
+gammas <- c(sapply(seq(0.5, 0.95, 0.05), log), log(0.94))
 
-t.test(effecttest_no_mhw, effecttest_mhw) 
+pwrout_gamma <- foreach(surv=survey_names$survey, .combine='rbind') %:% 
+  foreach(a = seq(1, iters, 1), .combine='rbind') %dopar% {
+    fn_sim_gamma(powerdat=powerdat, surv=surv, gammas=gammas, a=a)
+  }
+
+# analyze simulated data 
+sim_test_gamma <- NULL
+for(j in gammas){
+  for(i in 1:iters){
+    tmp11 <- pwrout_gamma %>% 
+      filter(gamma==j,
+             iter==i) 
+    tmp11_no <- tmp11 %>% filter(wt_mt_log > -Inf, wt_mt_log < Inf, mhw_yes_no == 0) %>% pull(wt_mt_log)
+    tmp11_yes <- tmp11 %>% filter(wt_mt_log > -Inf, wt_mt_log < Inf, mhw_yes_no == 1) %>% pull(wt_mt_log)
+    tmp11_t <- t.test(tmp11_no, tmp11_yes)
+    if(!class(tmp11_t)=="try-error"){
+      tmpdat2 <- tibble(t = tmp11_t$statistic, df = tmp11_t$parameter, p.value = tmp11_t$p.value, mean_no_mhw = mean(tmp11_no), mean_mhw = mean(tmp11_yes), sd_no_mhw = sd(tmp11_no), sd_mhw = sd(tmp11_yes), gamma=j, iter=i)
+    } else{
+      tmpdat2 <- tibble(t = NA, df = NA, p.value = NA, mean_no_mhw = NA, mean_mhw = NA, sd_no_mhw = NA, sd_mhw = NA, gamma = j, iter=i)
+    }
+    sim_test_gamma <- rbind(sim_test_gamma, tmpdat2)
+  }
+}
+
+sim_test_summ_gamma <- sim_test_gamma %>% 
+  mutate(exp_gamma = exp(gamma)) %>% 
+  group_by(exp_gamma) %>% 
+  summarise(propsig <- length(p.value[p.value<=0.05])/length(p.value)) 
